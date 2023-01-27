12ec1a85ab67e723553312cf8080afefda8113ba
YaszMoon <yasminite@gmail.com>
2023-01-27 19:30:39 +0000

    Add click event for search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 // Search
 // When search button clicked
 // Input added to button and prepended to area below button
+$("#search-button").on("click", function (event) {
+  event.preventDefault();
+  var newButton = $('<button type="button">');
+  var searchInput = $("#search-input").val();
+  newButton.text(searchInput);
+
+  $("#history").prepend(newButton);
+});
+
+// Add code for saving history to local storage
 
 // When city button clicked
 // Open weather api searched
