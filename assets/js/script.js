17966f34804d1ee27a0a57784b0702f1a7fcc5c4
YaszMoon <yasminite@gmail.com>
2023-01-27 19:16:05 +0000

    Add pseudo-code

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Search
+// When search button clicked
+// Input added to button and prepended to area below button
+
+// When city button clicked
+// Open weather api searched
+// Reulsts appended to current weather and forecast sections
