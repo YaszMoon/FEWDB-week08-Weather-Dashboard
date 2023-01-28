5de9f015c722cb18625ce83484de66082001e2e0
YaszMoon <yasminite@gmail.com>
2023-01-28 20:31:05 +0000

    Add starter code for city button click event

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,9 @@
 // Input added to button and prepended to area below button
 $("#search-button").on("click", function (event) {
   event.preventDefault();
-  var newButton = $('<button type="button">');
+  var newButton = $('<button type="button" class="cityButton">');
   var searchInput = $("#search-input").val();
+  newButton.attr('data-city', searchInput)
   newButton.text(searchInput);
 
   $("#history").prepend(newButton);
@@ -15,3 +16,17 @@ $("#search-button").on("click", function (event) {
 // When city button clicked
 // Open weather api searched
 // Reulsts appended to current weather and forecast sections
+$(".list-group").on("click", 'button.cityButton', function() {
+    console.log("here")
+
+    var city = $(this).attr('data-city');
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5fecb9a931136489a4d6d7443df317a7"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function(response) {
+            console.log(response)
+        })
+})
