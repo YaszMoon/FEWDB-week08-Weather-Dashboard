2dd192fb946af42a3c5f927c9179d070a9a04f7f
YaszMoon <yasminite@gmail.com>
2023-01-30 23:22:56 +0000

    Add text for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ $("#search-button").on("click", function (event) {
   event.preventDefault();
   var newButton = $('<button type="button" class="cityButton">');
   var searchInput = $("#search-input").val();
-  newButton.attr('data-city', searchInput)
+  newButton.attr("data-city", searchInput);
   newButton.text(searchInput);
 
   $("#history").prepend(newButton);
@@ -16,26 +16,43 @@ $("#search-button").on("click", function (event) {
 // When city button clicked
 // Open weather api searched
 // Reulsts appended to current weather and forecast sections
-$(".list-group").on("click", 'button.cityButton', function() {
-    console.log("here")
+$(".list-group").on("click", "button.cityButton", function () {
+  console.log("here");
 
-    var city = $(this).attr('data-city');
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5fecb9a931136489a4d6d7443df317a7"
+  var city = $(this).attr("data-city");
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=5fecb9a931136489a4d6d7443df317a7";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function(response) {
-            console.log(response)
-        })
-})
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
 
+    var todayDate = moment().format("D MMM YYYY");
+    var todayCity = $("<h2 class='mt-1 h3'>");
+    todayCity.text(city + " " + todayDate);
+    // Add icon to the above
+    
+    var currentTemp = $("<p>");
+    var conversion = response.main.temp - 273.15
+    conversion = conversion.toFixed(2)
+    currentTemp.text('Temperature: ' + conversion + ' °C')
 
-// h2 name of city, current date and icon
-// p temp
-// p wind
-// p humidity
+    var currentWind = $("<p>");
+    currentWind.text('Wind: ' + response.wind.speed + 'KpH')
+
+    var currentHumidity = $("<p>");
+    currentHumidity.text('Humidity: ' + response.main.humidity + '%')
+
+    $('#today').empty()
+    $("#today").append(todayCity, currentTemp, currentWind, currentHumidity);
+
+    
+  });
+});
 
 // 5-day Forecast
 // Some kind of array to loop through?
